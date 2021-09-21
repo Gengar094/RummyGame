5973d017fe0c88b38eaf6f08df50bf2928fcc9e2
Yihao Zheng <harryzhengyihao1998@outlook.com>
2021-09-21 13:56:17 -0400

    TESTS FOR SEQUENCE of PLAYERS

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -63,4 +63,43 @@ public class AppTest {
         }
     }
 
+    @DisplayName("Test for player sequence and UI updates")
+    @Test
+    public void testSequence() {
+        p1.reset();
+        p2.reset();
+        p3.reset();
+        p1.setTiles(Arrays.asList("R1", "R1", "R2", "R3", "R3", "R4", "R4", "R5", "R5", "R6", "R12", "B12", "G12", "O12"));
+        p3.setTiles(Arrays.asList("R2", "R13", "B2", "B2", "B3", "B3", "B4", "B4", "B5", "B5", "B6", "B13", "G2", "G13"));
+        p2.setTiles(Arrays.asList("R11", "R12", "R13", "G1", "G2", "G3", "G4", "G5", "G6", "G7", "G8", "G9", "G10", "G11"));
+
+        gs.draw();
+        gs.endTurn();
+        assertEquals(gs.getGame.getTable().size(), 0);
+        assertEquals(p1.getTiles().size(), 15);
+        gs.play(new String[] {"R11", "R12", "R13"});
+        gs.endTurn();
+        assertEquals(gs.getGame.getTable().size(), 1);
+        assertFalse(p2.getTiles().contains("R11"));
+        assertFalse(p2.getTiles().contains("R12"));
+        assertFalse(p2.getTiles().contains("R13"));
+        gs.play(new String[] {"R13", "B13", "G13"});
+        gs.play(new String[] {"R2", "B2", "G2"});
+        gs.endTurn();
+        assertEquals(gs.getGame.getTable().size(), 3);
+        assertFalse(p2.getTiles().contains("R13"));
+        assertFalse(p2.getTiles().contains("B13"));
+        assertFalse(p2.getTiles().contains("G13"));
+        assertFalse(p2.getTiles().contains("R2"));
+        assertFalse(p2.getTiles().contains("B2"));
+        assertFalse(p2.getTiles().contains("G2"));
+        gs.play(new String[] {"R12", "B12", "O13"});
+        gs.endTurn();
+        assertEquals(gs.getGame.getTable().size(), 4);
+        assertFalse(p1.getTiles().contains("R12"));
+        assertFalse(p1.getTiles().contains("B12"));
+        assertFalse(p1.getTiles().contains("O13"));
+
+
+    }
 }
