8623e2313cea1283e509e7acaa223a3ee0672cd8
Yihao Zheng <harryzhengyihao1998@outlook.com>
2021-11-19 04:24:51 -0500

    Add more cases for play

--- src/test/java/JUnit/ParameterizedAppTest/playTest.java
+++ src/test/java/JUnit/ParameterizedAppTest/playTest.java
@@ -9,10 +9,13 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -59,7 +62,9 @@ public class playTest {
             "R8,B8,G8,O8",
             "R1,R2,R3/R8,B8,G8",
             "R10,B10,*",
-            "R11,R12,*"})
+            "R8,B8,G8,*",
+            "R11,R12,*",
+            "B7,B8,B9,*"})
     public void test_play_for_initial_30(String melds) {
         // Given
         String[] ss = melds.split("/");
@@ -95,7 +100,9 @@ public class playTest {
             "R5,B5,G5",
             "R6,B6,G6,O6",
             "R1,R2,*",
-            "R7,B7,*"})
+            "R4,R5,R6,*",
+            "R7,B7,*",
+            "R3,B3,G3,*"})
     public void test_play_valid_meld_after_initial_30(String melds) {
         // Given
         String[] ss = melds.split("/");
@@ -129,7 +136,9 @@ public class playTest {
     @ParameterizedTest
     @ValueSource(strings =
             {"R10,R11,R11,R13",
+            "R10,R11,R13",
             "R7,B7,G7,O6",
+            "R7,B7,O6",
             "R7,B6,*" ,
             "R10,R11,R11,*"})
     public void test_play_an_invalid_meld_general(String melds) {
@@ -163,7 +172,11 @@ public class playTest {
     @ParameterizedTest
     @ValueSource(strings =
             {"R10,B11,R12,R13",
-            "R7,B7,G7,G7"})
+             "R10,R11,G12",
+             "R7,B7,G7,G7",
+             "R7,R7,B7",
+            "R10,B11,R12,*",
+            "R10,R10,*"})
     public void test_play_an_invalid_meld_wrong_color(String melds) {
         // Given
         String[] ss = melds.split("/");
@@ -226,23 +239,31 @@ public class playTest {
     }
 
 
-    @Test
-    public void test_a_tile_that_player_does_not_have() {
+    @ParameterizedTest
+    @MethodSource
+    public void test_a_tile_that_player_does_not_have(String doesNotHave, String has, String play) {
         // Given
-        while (gs.getCurrentPlayer().getTiles().contains("R5")) {
-            gs.getCurrentPlayer().getTiles().remove("R5");
-            Config.tiles.add("R5");
+        while (gs.getCurrentPlayer().getTiles().contains(doesNotHave)) {
+            gs.getCurrentPlayer().getTiles().remove(doesNotHave);
+            Config.tiles.add(doesNotHave);
         }
 
-        gs.setDesiredAndUniqueTiles(new String[] {"R6", "R7"});
+        gs.setDesiredAndUniqueTiles(has.split(","));
 
         // When
-        gs.play(new String[] {"R5", "R6", "R7"});
+        gs.play(play.split(","));
 
         // Then
-        assertFalse(gs.getGame().getTable().contains(new ArrayList<>(Arrays.asList("R5","R6","R7"))));
-        assertTrue(gs.getCurrentPlayer().getTiles().contains("R6"));
-        assertTrue(gs.getCurrentPlayer().getTiles().contains("R7"));
+        assertFalse(gs.getGame().getTable().contains(new ArrayList<>(Arrays.asList(play.split(",")))));
+        for (String s: has.split(",")) {
+            assertTrue(gs.getCurrentPlayer().getTiles().contains(s));
+        }
+    }
+
+    static List<Arguments> test_a_tile_that_player_does_not_have() {
+        return List.of(
+                Arguments.arguments("R5", "R6,R7", "R5,R6,R7")
+        );
     }
 
     @ParameterizedTest
@@ -252,7 +273,9 @@ public class playTest {
             "R9,B9,O9",
             "R7,B7,G7,O7",
             "R5,R6,R7,*",
+            "R8,R9,*",
             "R7,B7,G7,*",
+            "R9,B9,*",
             "R1,B1,G1/R1,R2,R3,R4"})
     public void test_play_valid_meld_but_invalid_for_initial_30(String melds) {
         // Given
